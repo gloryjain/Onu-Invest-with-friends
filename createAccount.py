18dbfae2a5e48b9fe3f03b92739330728ad28db8
Kimberly Santiago <kdsanman@gmail.com>
2017-05-25 13:02:12 -0400

    Fixed account creation python file to ask numbers in command line

--- createAccount.py
+++ createAccount.py
@@ -5,13 +5,15 @@ import json
 import random
 
 def createAccount(customerId, customer):
-    customerId = '5925e707a73e4942cdafd635'
-    customer = 'Deondre'
-    apiKey = '508de63e607d501fc1617f4e39315b86'
+    customerId = customerId
+    customer = customer
+    #Glorys API key
+    apiKey = '89e1407d751d9033c3bf258c76a33e79'
     accountType = 'Checking'
 
     url = 'http://api.reimaginebanking.com/customers/{}/accounts?key={}'.format(customerId,apiKey)
 
+    #Setting up account number at random 16-digit format
     accountNumber = str(random.randint(1000000000000000, 9999999999999999))
 
     payload = {
@@ -30,3 +32,16 @@ def createAccount(customerId, customer):
 
     if response.status_code == 201:
         print('account created for', customer)
+
+    return response.status_code == 201
+
+def main():
+    customerId = raw_input("To create an account, please enter a customer ID: ")
+    customerName = raw_input("What is the customer's name?: ")
+    if createAccount(customerId, customerName):
+        print('Account creation for', customer, 'was successful')
+    else:
+        print('Error creating account, please try again')
+    
+
+main()
