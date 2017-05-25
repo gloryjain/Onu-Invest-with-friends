99be32f45da90dd53b494324a49aecc44a0fd0aa
Glory Jain <glojain@umich.edu>
2017-05-25 14:37:20 -0400

    desposityMoney

--- deposityMoney.py
+++ deposityMoney.py
@@ -1,3 +1,28 @@
+#Function to withraw money from the individual's bank
+#and then deposit in the central Onu Bank
+#will then subtract from the Onu bank
+#Also keeps track how much 
+
+import requests
+import json
+import random
+
+moneyToWithdraw = 0
+
+glory = 592713b4ceb8abe24250de24
+kobi = 592713c2ceb8abe24250de28
+ben = 592713bfceb8abe24250de27
+kyle = 592713bcceb8abe24250de26 
+kim = 592713baceb8abe24250de25
+
+group = {"glory":glory,"kobi":kobi,"ben":ben, "kyle":kyle, "kim":kim}
+
+def changeMoney(money):
+	moneyToWithdraw = money
+
+def withdrawIndiv(customerID):
+	for key, value in group.iterItems():
+		print(key, value)
 
 
-def 
+def depositOnu(total):
