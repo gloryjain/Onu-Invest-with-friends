ff6a6ad4e553f9a4537d43ea58be217849a4c3ce
Kyle Feng <kylefeng28@gmail.com>
2017-05-25 14:37:15 -0400

    Add fallback dataset for stock prices

--- Tools/stock_price.py
+++ Tools/stock_price.py
@@ -1,6 +1,7 @@
 import locale
-import quandl
 from datetime import *
+import quandl
+import matplotlib.pyplot as plt
 
 locale.setlocale(locale.LC_ALL, ('en_US', 'UTF-8')) # Set locale to en_US
 quandl.ApiConfig.api_key = "V5uEXA4L1zfc9Q6Dp9Lz" # Set API key
@@ -13,7 +14,10 @@ def get_stock_info(ticker, dataset, suffix, start_date='', end_date=''):
 def get_stock_price(ticker):
     # Wiki prices
     yesterday = (datetime.now() - timedelta(days=1)).strftime('%Y-%m-%d')
-    data = get_stock_info(ticker, dataset='EOD', suffix='', start_date=yesterday)
+    try:
+        data = get_stock_info(ticker, dataset='EOD', suffix='', start_date=yesterday)
+    except quandl.errors.quandl_error.ForbiddenError:
+        data = get_stock_info(ticker, dataset='WIKI', suffix='', start_date=yesterday)
     return data.tail(1)['Close'].tolist()[0]
 
 def plot_stock_earnings(ticker):
@@ -21,21 +25,25 @@ def plot_stock_earnings(ticker):
     # Earnings per Basic Share (Most Recent - Quarterly)
     data = get_stock_info(ticker, dataset='SF1', suffix='_EPS_MRQ')
     print data
+    data.plot()
+    # plot
     return data
 
 # Returns stock price in a user-friendly way
 # https://stackoverflow.com/questions/320929/currency-formatting-in-python
 def get_stock_price_friendly(ticker):
-    ticker = ticker.upper()
     try:
         price = get_stock_price(ticker)
         price_pretty = locale.currency(price)
-        return "Yesterday's end-of-day price for %s is %s." % (ticker, price_pretty)
+        return "Yesterday's end-of-day price for %s is %s." % (ticker.upper(), price_pretty)
     except quandl.errors.quandl_error.NotFoundError as e:
         # print e # debug
         return "Sorry, %s is not a valid ticker symbol name." % ticker
 
 # debug
 print get_stock_price_friendly('AAPL')
+print get_stock_price_friendly('GOOG')
+print get_stock_price_friendly('FB')
+print get_stock_price_friendly('COF')
 print get_stock_price_friendly('QWERTY')
 # plot_stock_earnings('AAPL')
