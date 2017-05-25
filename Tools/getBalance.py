209d18ceee3bccf9c50c52701a8aa890abf1a553
Kimberly Santiago <kdsanman@gmail.com>
2017-05-25 14:55:51 -0400

    renamed moneyInBank.py to getBalance.py based on name of fnction

--- Tools/getBalance.py
+++ Tools/getBalance.py
@@ -4,25 +4,21 @@ import requests
 import json
 import random
 
-def getBalance():
-    customerId = '5925e707a73e4942cdafd635'
+def getBalance(customerId):
     #Glorys API key
     apiKey = '89e1407d751d9033c3bf258c76a33e79'
-    apiKey = '508de63e607d501fc1617f4e39315b86' #kims
+    #apiKey = '508de63e607d501fc1617f4e39315b86' #kims
     
     url = 'http://api.reimaginebanking.com/accounts?type=Checking&key={}'.format(apiKey)
 
-    response = requests.get(
-        url,
-        )
+    response = requests.get(url,)
 
     list_response = list(response.json())
     
     for i in range(0,len(list_response)):
         information_dic = list_response[i]
-        print information_dic.keys()
-        if information_dic['customer_id'] == int(customerId):
-            print 
+        if information_dic['customer_id'] == customerId and information_dic['type'] == 'Checking':
+            return information_dic['balance']
 
-
-getBalance()
+customerId = '5926f38da73e4942cdafd65b'
+print(getBalance(customerId))
